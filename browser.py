49be1a836f1254e4d29e91e41734c23faf6ff8cd
Minseong Kim <jja08111@gmail.com>
2025-10-08 21:06:13 +0900

    Implement 2-3

--- browser.py
+++ browser.py
@@ -12,11 +12,12 @@ class Browser:
       width=WIDTH,
       height=HEIGHT,
     )
-    self.canvas.pack()
+    self.canvas.pack(fill=tkinter.BOTH, expand=True)
     self.scroll = 0
     self.window.bind("<Up>", self.scrollup)
     self.window.bind("<Down>", self.scrolldown)
     self.window.bind("<MouseWheel>", self.on_mouse_wheel)
+    self.window.bind("<Configure>", self.on_configure)
 
   def _scroll_internal(self, delta: float):
     self.scroll -= delta
@@ -36,18 +37,25 @@ class Browser:
     elif e.delta < 0:
       self._scroll_internal(e.delta)
 
+  def on_configure(self, _):
+    self.layout_and_display()
+
   def draw(self):
     self.canvas.delete("all")
+    height = self.canvas.winfo_height()
     for x, y, c in self.display_list:
-      if y > self.scroll + HEIGHT or y + VSTEP < self.scroll:
+      if y > self.scroll + height or y + VSTEP < self.scroll:
         continue
       self.canvas.create_text(x, y - self.scroll, text=c)
   
+  def layout_and_display(self):
+    self.display_list = layout(self.text, self.canvas.winfo_width())
+    self.draw()
+
   def load(self, url: URL):
     body = url.request()
-    text = lex(body)
-    self.display_list = layout(text)
-    self.draw()
+    self.text = lex(body)
+    self.layout_and_display()
 
 if __name__ == "__main__":
   import sys
--- layout.py
+++ layout.py
@@ -1,6 +1,4 @@
-
 from typing import List
-from constant import WIDTH
 
 HSTEP, VSTEP = 13, 18
 NEWLINE_STEP = 24
@@ -10,13 +8,13 @@ class DisplayList:
   y: int
   c: str
 
-def layout(text: str) -> List[DisplayList]:
+def layout(text: str, width: int) -> List[DisplayList]:
   display_list: List[DisplayList] = []
   cursor_x, cursor_y = HSTEP, VSTEP
   for c in text:
     display_list.append((cursor_x, cursor_y, c))
     cursor_x += HSTEP
-    if cursor_x >= WIDTH - HSTEP:
+    if cursor_x >= width - HSTEP:
       cursor_x = HSTEP
       cursor_y += VSTEP
     elif c == "\n":
