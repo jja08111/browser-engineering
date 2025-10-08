916ec38821cd7b1f3612c111e64edda397e8820c
Minseong Kim <jja08111@gmail.com>
2025-10-08 22:08:09 +0900

    Implement 2-4

--- browser.py
+++ browser.py
@@ -1,5 +1,5 @@
 import tkinter
-from constant import HEIGHT, SCROLL_STEP, WIDTH
+from constant import HEIGHT, SCROLL_STEP, SCROLLBAR_PADDING, SCROLLBAR_WIDTH, WIDTH
 from layout import VSTEP, layout
 from lexer import lex
 from url import URL
@@ -19,11 +19,24 @@ class Browser:
     self.window.bind("<MouseWheel>", self.on_mouse_wheel)
     self.window.bind("<Configure>", self.on_configure)
 
+  def _content_max_y(self):
+    return self.display_list[-1].y if self.display_list.__len__() else 0
+
   def _scroll_internal(self, delta: float):
     self.scroll -= delta
+    max_y = self._content_max_y()
     if self.scroll < 0:
       self.scroll = 0
-    self.draw()
+    if self.scroll + self._window_height() > max_y:
+      self.scroll = max_y - self._window_height()
+    self.draw_content()
+    self.draw_scrollbar()
+  
+  def _window_height(self):
+    return self.canvas.winfo_height()
+  
+  def _window_width(self):
+    return self.canvas.winfo_width()
 
   def scrollup(self, _):
     self._scroll_internal(-SCROLL_STEP)
@@ -38,24 +51,38 @@ class Browser:
       self._scroll_internal(e.delta)
 
   def on_configure(self, _):
-    self.layout_and_display()
+    self.layout_and_draw()
 
-  def draw(self):
+  def draw_scrollbar(self):
+    max_y = self._content_max_y()
+    window_height = self._window_height()
+    window_width = self._window_width()
+    if window_height > max_y:
+      return
+    scrollbar_height = window_height * (window_height / max_y)
+    x = window_width - SCROLLBAR_WIDTH - SCROLLBAR_PADDING
+    current_scroll_percentage = self.scroll / (max_y - window_height)
+    y = (window_height - scrollbar_height - 2 * SCROLLBAR_PADDING) * current_scroll_percentage
+    y += SCROLLBAR_PADDING
+    self.canvas.create_rectangle(x, y, x + SCROLLBAR_WIDTH, y + scrollbar_height, fill="gray")
+
+  def draw_content(self):
     self.canvas.delete("all")
-    height = self.canvas.winfo_height()
+    height = self._window_height()
     for x, y, c in self.display_list:
       if y > self.scroll + height or y + VSTEP < self.scroll:
         continue
       self.canvas.create_text(x, y - self.scroll, text=c)
-  
-  def layout_and_display(self):
-    self.display_list = layout(self.text, self.canvas.winfo_width())
-    self.draw()
+
+  def layout_and_draw(self):
+    self.display_list = layout(self.text, self._window_width())
+    self.draw_content()
+    self.draw_scrollbar()
 
   def load(self, url: URL):
     body = url.request()
     self.text = lex(body)
-    self.layout_and_display()
+    self.layout_and_draw()
 
 if __name__ == "__main__":
   import sys
--- layout.py
+++ layout.py
@@ -3,16 +3,28 @@ from typing import List
 HSTEP, VSTEP = 13, 18
 NEWLINE_STEP = 24
 
-class DisplayList:
+class DisplayItem:
   x: int
   y: int
   c: str
 
-def layout(text: str, width: int) -> List[DisplayList]:
-  display_list: List[DisplayList] = []
+  def __init__(self, x: int, y: int, c: str):
+    self.x = x
+    self.y = y
+    self.c = c
+  
+  def __iter__(self):
+    yield self.x
+    yield self.y
+    yield self.c
+
+# TODO: Consider content view width because of scrollbar?
+def layout(text: str, width: int) -> List[DisplayItem]:
+  display_list: List[DisplayItem] = []
   cursor_x, cursor_y = HSTEP, VSTEP
   for c in text:
-    display_list.append((cursor_x, cursor_y, c))
+    display_item = DisplayItem(x=cursor_x, y=cursor_y, c=c)
+    display_list.append(display_item)
     cursor_x += HSTEP
     if cursor_x >= width - HSTEP:
       cursor_x = HSTEP
